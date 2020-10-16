b41d10e7a37dd6bb24b6b893a20610aea4d8519a
Roman Almazov <ralmaz.vue@ya.ru>
2020-10-16 20:46:05 +0300

    last lesson

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <SomeComponent />
+  </div>
+</template>
+
+<script>
+import SomeComponent from '@/components/SomeComponent.vue'
+
+export default {
+  components: {
+    SomeComponent
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
